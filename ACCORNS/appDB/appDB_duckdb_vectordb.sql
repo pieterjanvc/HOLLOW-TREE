5db3f0454ea89b976287810e963bcd934ad6e086
PJ <52793278+pieterjanvc@users.noreply.github.com>
2024-06-03 13:59:22 -0400

    Clean up DB creation code (#44)
    - New functions have been parsed out to make it easier to create the various DB and add demo data
    - Adding demo data now in the shared settings, since it is relevant for both apps

--- ACCORNS/appDB/appDB_duckdb_vectordb.sql
+++ ACCORNS/appDB/appDB_duckdb_vectordb.sql
@@ -1,3 +1,13 @@
+DROP TABLE IF EXISTS "documents";
+CREATE TABLE "documents" (
+  "node_id" VARCHAR, 
+  "text" VARCHAR, 
+  "embedding" FLOAT[], 
+  "metadata_" JSON
+  );
+
+DROP SEQUENCE IF EXISTS seq_fID;
+DROP TABLE IF EXISTS metadata;
 CREATE SEQUENCE seq_fID START 1;
 CREATE TABLE file (
   "fID" INTEGER PRIMARY KEY,
@@ -8,6 +18,8 @@ CREATE TABLE file (
   "modified" TEXT
 );
 
+DROP SEQUENCE IF EXISTS seq_kID;
+DROP TABLE IF EXISTS keyword;
 CREATE SEQUENCE seq_kID START 1;
 CREATE TABLE keyword (
   "kID" INTEGER PRIMARY KEY,
